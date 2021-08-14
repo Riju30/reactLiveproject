46466e1a5fd90bf2de9bff9e58bca400c2129a12
unknown <riju30biswas@gmail.com>
2021-08-14 15:47:12 +0530

    footer solved

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     const year = new Date().getFullYear();
     return (
         <>
-            <footer className="bg-light text-center">
+            <footer className="bg-light text-center mt-5 mt-sm-5 mt-md-5 mt-xl-5 mt-lg-5">
                 <p>©{year} Global Web. All Rights Reserved | Terms and Condition</p>
             </footer>
         </>
